c5dc6eb397c722e3b0d73914d6bcad3ffd29bd13
Kelpie <ariel.p20@mhs.istts.ac.id>
2020-12-06 00:33:13 +0800

    asd

--- bot.js
+++ bot.js
@@ -1,15 +1,5 @@
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+require("heroku.js");
+update();
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
